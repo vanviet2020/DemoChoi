1dc556389130b0837208e0bc3fb7c64526871416
Nguyen Van Viet <3120518051@ued.udn.vn>
2021-04-03 13:15:58 +0700

    first commit

---
+++ Demo_MVC/Controllers/UsersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Demo_MVC.Models;
+
+namespace Demo_MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private MyDBDemoEntities db = new MyDBDemoEntities();
+
+        // GET: Users
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.tblUsers.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = await db.tblUsers.FindAsync(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,name,email,address")] tblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblUsers.Add(tblUser);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblUser);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = await db.tblUsers.FindAsync(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,name,email,address")] tblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblUser).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(tblUser);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = await db.tblUsers.FindAsync(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            tblUser tblUser = await db.tblUsers.FindAsync(id);
+            db.tblUsers.Remove(tblUser);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
