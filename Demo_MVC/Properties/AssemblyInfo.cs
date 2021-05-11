24239c489ffd46424ade2ed4df3a612da072375c
yen <hoanghaiyen12620@gmail.com>
2021-05-12 00:16:12 +0700

    tao mới thêm vào đó

--- Demo_MVC/Properties/AssemblyInfo.cs
+++ Demo_MVC/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+//tao mới sửa
 [assembly: AssemblyTitle("Demo_MVC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
